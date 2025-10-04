7f1301d852c4b3b08374582549c629e837ac43c4
carledora537 <carl.edora.537@my.csun.edu>
2025-10-03 21:56:26 -0700

    Update Product.java

--- Product.java
+++ Product.java
@@ -5,7 +5,17 @@ public class Product {
     private String name;
     private String description;
     private double price;
+    private int quantity;
     private boolean available;
 
-    // Constructors, Getters, Setters
+    // Constructors
+    public Product() {}
+    public Product(Long id, String name, String description, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.available = quantity > 0;
+    }
 }
