5fea74391c99941c386f36b78035a45c1fdc283f
carledora537 <carl.edora.537@my.csun.edu>
2025-10-03 21:50:56 -0700

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,11 @@
+package com.abc.ecommerce.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean available;
+
+    // Constructors, Getters, Setters
+}
