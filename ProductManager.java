f3b9f00858d9270e7a21fbcb717a55e5ce05ab8e
carledora537 <carl.edora.537@my.csun.edu>
2025-10-17 21:34:36 -0700

    Update and rename EcommerceApplication.java to ProductManager.java

---
+++ ProductManager.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Bouquet;
+import java.util.List;
+
+public class ProductManager {
+    private List<Bouquet> catalog;
+
+    public ProductManager(List<Bouquet> catalog) {
+        this.catalog = catalog;
+    }
+
+    public void addProduct(Bouquet bouquet) {
+        catalog.add(bouquet);
+    }
+
+    public boolean removeProductById(String id) {
+        return catalog.removeIf(b -> b.getId().equals(id));
+    }
+}
