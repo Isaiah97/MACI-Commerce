b0d5c043a195bc2afcccd4fb8aaf917470c05fa8
carledora537 <carl.edora.537@my.csun.edu>
2025-10-28 15:58:20 -0700

    Implement EmailService for order confirmation

---
+++ EmailService.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.Order;
+
+public class EmailService {
+    public void sendConfirmation(String toEmail, Order order) {
+        String subject = "🌸 Your Bouquet Order Confirmation";
+        String body = "Dear Customer,\n\nThank you for your purchase!\n\n" +
+                      order.toString() + "\n\nWe hope your flowers bring joy!\n\nBouquet Sales Co.";
+        System.out.println("Sending email to " + toEmail + "...\nSubject: " + subject + "\n\n" + body);
+    }
+}
