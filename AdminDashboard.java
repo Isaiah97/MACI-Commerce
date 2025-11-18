785cff98739045a972d6379801607dc3915d0b2c
carledora537 <carl.edora.537@my.csun.edu>
2025-11-17 18:01:39 -0800

    Create AdminDashboard.java

---
+++ AdminDashboard.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Bouquet;
+import model.Order;
+import java.util.List;
+
+public class AdminDashboard {
+    public void displayCatalog(List<Bouquet> catalog) {
+        System.out.println("📦 Product Catalog:");
+        catalog.forEach(System.out::println);
+    }
+
+    public void displayOrders(List<Order> orders) {
+        System.out.println("📋 Incoming Orders:");
+        orders.forEach(System.out::println);
+    }
+}
