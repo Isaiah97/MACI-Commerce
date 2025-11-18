ce974be8a0d6fefa6f205259305d23fb00ddbb49
carledora537 <carl.edora.537@my.csun.edu>
2025-11-17 18:00:50 -0800

    Implement AuthService for admin authentication

---
+++ AuthService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.Admin;
+
+public class AuthService {
+    private final Admin admin = new Admin("admin", "secure123");
+
+    public boolean authenticate(String username, String password) {
+        return admin.getUsername().equals(username) && admin.getPassword().equals(password);
+    }
+}
