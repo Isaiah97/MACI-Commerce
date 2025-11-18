0ad69b5419704a5ad9f53ac013f479750e57f208
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-18 14:28:25 -0800

    added statuses to delivery statuses

--- DeliveryStatus.java
+++ DeliveryStatus.java
@@ -1,8 +1,20 @@
 package model;
 
 public enum DeliveryStatus {
-    NOT_SHIPPED,
-    IN_TRANSIT,
-    DELIVERED,
-    FAILED_TO_UPDATE
+    NOT_SHIPPED("Package has not been shipped"),
+    IN_TRANSIT("Package is in transit"),
+    DELIVERED("Package has been delivered"),
+    FAILED_TO_UPDATE("Tracking is unavailable");
+
+    private final String message;
+
+    DeliveryStatus (String msg){
+        this.message = msg;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+
 }
