fbf19db9b5de61960531faf118abfa2d3545efab
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-18 14:41:22 -0800

    error comments added

--- DeliveryTracker.java
+++ DeliveryTracker.java
@@ -1,24 +1,24 @@
 package service;
 
-import model.*;
+import model.*; //error
 
 public class DeliveryTracker {
-    public void updateDeliveryStatus(Order order) {
-        if (order.getStatus() == OrderStatus.PAID) {
-            order.setDeliveryStatus(DeliveryStatus.IN_TRANSIT);
+    public void updateDeliveryStatus(Order order) { //error
+        if (order.getStatus() == OrderStatus.PAID) { //error
+            order.setDeliveryStatus(DeliveryStatus.IN_TRANSIT); //error 
         } else {
-            order.setDeliveryStatus(DeliveryStatus.FAILED_TO_UPDATE);
+            order.setDeliveryStatus(DeliveryStatus.FAILED_TO_UPDATE); //error
         }
     }
 
-    public void markAsDelivered(Order order) {
-        if (order.getDeliveryStatus() == DeliveryStatus.IN_TRANSIT) {
-            order.setDeliveryStatus(DeliveryStatus.DELIVERED);
-            order.setStatus(OrderStatus.DELIVERED);
+    public void markAsDelivered(Order order) { //error
+        if (order.getDeliveryStatus() == DeliveryStatus.IN_TRANSIT) { //error
+            order.setDeliveryStatus(DeliveryStatus.DELIVERED); //error
+            order.setStatus(OrderStatus.DELIVERED); //error
         }
     }
 
-    public void printTrackingInfo(Order order) {
+    public void printTrackingInfo(Order order) { //error
         System.out.println("📦 Tracking Info for Order " + order.getOrderId() + ": " + order.getDeliveryStatus());
     }
 }
