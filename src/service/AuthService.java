ec5d3f4f082fad63b2dfca176fe31b385a01f644
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-22 12:16:16 -0800

    transfered code over

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,10 +1,17 @@
 package service;
 
+import model.*;
 
 /**
 *TODO: Implemnts authentication logic
 */
 
 public class AuthService {
+    private final Admin admin = new Admin("admin", "secure123");
 
+    public boolean authenticate(String username, String password) {
+        return admin.getUsername().equals(username) && admin.getPassword().equals(password);
+    }
 }
+
+
