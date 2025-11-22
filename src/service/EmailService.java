528cc85fb572c32adf432c1c4a5d6e95d8fd2c34
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-22 12:22:01 -0800

    email services functional

--- src/service/EmailService.java
+++ src/service/EmailService.java
@@ -1,1 +1,17 @@
+package service;
+
+import model.*;
+
+/**
+ * TODO: Implement email sending logic
+ */
+
+public class EmailService {
+    public void sendConfirmation(String toEmail, Order order) {
+        String subject = "🌸 Your Bouquet Order Confirmation";
+        String body = "Dear Customer,\n\nThank you for your purchase!\n\n" +
+                      order.toString() + "\n\nWe hope your flowers bring joy!\n\nBouquet Sales Co.";
+        System.out.println("Sending email to " + toEmail + "...\nSubject: " + subject + "\n\n" + body);
+    }
+}
 
