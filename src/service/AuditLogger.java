7910736c749e59862a9cfb41c8506a088f156811
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-22 12:51:56 -0800

    auditlogger transfer

--- src/service/AuditLogger.java
+++ src/service/AuditLogger.java
@@ -1,1 +1,17 @@
+package service;
+
+import model.*;
+import java.time.LocalDateTime;
+
+/**
+ * 
+ * TODO: audit logger implementation
+ * 
+ */
+
+public class AuditLogger {
+    public void log(String action, String actor) {
+        System.out.println("🔒 AUDIT LOG [" + LocalDateTime.now() + "] - " + actor + ": " + action);
+    }
+}
 
