8b5a6b3b61dd4b12ba73db41f746f8c7a47f973e
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-22 12:55:47 -0800

    dashboard transfer

--- src/service/AdminDashboard.java
+++ src/service/AdminDashboard.java
@@ -1,1 +1,24 @@
+package service;
+
+import model.Bouquet;
+import model.Order;
+import java.util.List;
+
+/**
+ * 
+ * TODO: dashboard
+ * 
+ */
+
+public class AdminDashboard {
+    public void displayCatalog(List<Bouquet> catalog) {
+        System.out.println("📦 Product Catalog:");
+        catalog.forEach(System.out::println);
+    }
+
+    public void displayOrders(List<Order> orders) {
+        System.out.println("📋 Incoming Orders:");
+        orders.forEach(System.out::println);
+    }
+}
 
