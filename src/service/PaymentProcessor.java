4a211d9cb51e55735f600ea9f2e4c187cea58570
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-22 12:24:06 -0800

    payment processing functional

--- src/service/PaymentProcessor.java
+++ src/service/PaymentProcessor.java
@@ -1,1 +1,15 @@
+package service;
+
+import model.*;
+
+
+/**
+ * TODO: Implement payment logic
+ */
+
+public class PaymentProcessor {
+    public boolean processPayment(String cardNumber, String expiry, String cvv, double amount) {
+        return cardNumber.startsWith("4") && amount > 0;
+    }
+}
 
