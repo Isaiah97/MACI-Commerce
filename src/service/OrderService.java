951024b9d24678fd9675a952a2191c72053ed677
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-11-22 12:27:18 -0800

    order service functional

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,1 +1,35 @@
+package service;
+
+import model.*;
+import java.util.*;
+
+/**
+ * 
+ * TODO: implementation of OrderService
+ */
+
+public class OrderService {
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public List<Order> getAllOrders() {
+        return orders;
+    }
+
+    public Optional<Order> getOrderById(String orderId) {
+        return orders.stream().filter(o -> o.getOrderId().equals(orderId)).findFirst();
+    }
+
+    public boolean updateOrderStatus(String orderId, OrderStatus status) {
+        Optional<Order> orderOpt = getOrderById(orderId);
+        if (orderOpt.isPresent()) {
+            orderOpt.get().setStatus(status);
+            return true;
+        }
+        return false;
+    }
+}
 
