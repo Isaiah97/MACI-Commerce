844762fa9958abc0c8767c2977881c908bb9b020
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-02 15:12:26 -0800

    Catalog Service building

--- src/service/CatalogService.java
+++ src/service/CatalogService.java
@@ -1,11 +1,24 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Bouquet;
+
 /**
  * 
  * TODO: implementation of catalog operation
  */
 
 public class CatalogService{
+
+	private final List<Bouquet> bouquets = new ArrayList<>();
+
+	public CatalogService() {
+
+		bouquets.add(new Bouquet("Spring Garden Bouquet", "Roses", 39.99));
+		bouquets.add(new Bouquet("Romantic Rose Dozen", "Roses", 49.99));
+        bouquets.add(new Bouquet("Lavender Dreams", "Mixed", 44.25));
+    }
 
 	public List<Bouquet> search (String name, String category, int minPrice, int maxPrice){
 		List<Bouquet> all = getAll();
