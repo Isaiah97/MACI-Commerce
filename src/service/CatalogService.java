b43ce5fbf165f14d404be1de7af6663d479bed92
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-02 14:54:37 -0800

    logic implementation

--- src/service/CatalogService.java
+++ src/service/CatalogService.java
@@ -6,5 +6,19 @@ package service;
  */
 
 public class CatalogService{
-	
+
+	public List<Bouquet> search (String name, String category, int minPrice, int maxPrice){
+		List<Bouquet> all = getAll();
+		List <Bouquet> results = new ArrayList<>();
+
+		for (Bouquet b : all) {
+			boolean matchesName = b.getName().contains(name);
+			boolean matchesCategory = b.getCategory().equalsIgnoreCase(category);
+			boolean inRange = b.getPrice() >= minPrice && b.getPrice() <= maxPrice;
+
+			if (matchesName && matchesCategory && inRange) {
+				results.add(b);
+			}
+	}
+	return results;
 }
