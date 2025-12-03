4fa71da92b898d9b5a01e224d24c03715af4c784
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-02 17:19:26 -0800

    finished GUIMain. still have more to do. taking a break

--- src/app/GUIMain.java
+++ src/app/GUIMain.java
@@ -5,5 +5,14 @@ import service.CatalogService;
 import service.OrderService;
 
 public class GUIMain {
-	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			CatalogService catalogService = new CatalogService();
+			OrderService orderService = new OrderService();
+
+			FloralShopFrame frame = new FloralShopFrame(CatalogService, OrderService);
+			frame.setVisible(true);
+		}
+	}
+
 }
