ebccced959a89757a4f712ef9aa8b8457d041e04
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-03 17:13:59 -0800

    initialization of gui set up potential fixes need to go thru

--- src/app/FloralShopFrame.java
+++ src/app/FloralShopFrame.java
@@ -47,7 +47,47 @@ public class FloralShopFrame {
 		title.setFont(title.getFont().deriveFont(Font.BOLD, 20f));
 		add(title, Border.NORTH);
 
-		
-	}
+		JPanel centerPanel = new JPanel(new GridLayout(1, 2, 10, 0));
 
+		JPanel catalogPanel = bew JPanel(new Border());
+		catalogPanel.setBorder(BorderFactory.createTitledBorder("Catalog"));
+		catalogList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        catalogPanel.add(new JScrollPane(catalogList), BorderLayout.CENTER);
+
+        JButton addToCartBtn = new JButton("Add to Order ➜");
+        addToCartBtn.addActionListener(this::onAddToOrder);
+        catalogPanel.add(addToCartBtn, BorderLayout.SOUTH);
+
+        JPanel cartPanel = new JPanel(new BorderLayout());
+        cartPanel.setBorder(BorderFactory.createTitledBorder("Current Order"));
+        cartList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        cartPanel.add(new JScrollPane(cartList), BorderLayout.CENTER);
+
+        centerPanel.add(catalogPanel);
+        centerPanel.add(cartPanel);
+
+        add(centerPanel, BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+
+        orderSummaryArea.setEditable(false);
+        orderSummaryArea.setBorder(BorderFactory.createTitledBorder("Order Summary"));
+        bottomPanel.add(new JScrollPane(orderSummaryArea), BorderLayout.CENTER);
+
+        JPanel buttonRow = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        JButton checkoutBtn = new JButton("Checkout");
+        checkoutBtn.addActionListener(this::onCheckout);
+
+        JButton adminBtn = new JButton("Admin Panel");
+        adminBtn.addActionListener(this::onAdminPanel);
+
+        buttonRow.add(adminBtn);
+        buttonRow.add(checkoutBtn);
+
+        bottomPanel.add(buttonRow, BorderLayout.SOUTH);
+
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
 }
+    
