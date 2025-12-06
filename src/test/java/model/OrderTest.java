6ad4c73b21d6c5f099c8325e531811fcc57c3746
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-05 21:52:43 -0800

    initializing testcase for Order

---
+++ src/test/java/model/OrderTest.java
@@ -0,0 +1,11 @@
+package model;
+
+import static org.junit.jupiter.api.Assertion.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OrderTest {
+	
+}
