c6a527c11f5f63930229ac703aa2b85e121a8b6f
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-05 22:37:41 -0800

    error fixes in GUIMain

--- src/app/GUIMain.java
+++ src/app/GUIMain.java
@@ -3,23 +3,21 @@ package app;
 import javax.swing.SwingUtilities;
 import service.CatalogService;
 import service.OrderService;
-import service.AuditLogger;  
+import service.AuditLogger;
 
 public class GUIMain {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             CatalogService catalogService = new CatalogService();
             OrderService orderService = new OrderService();
-
-			AuditLogger logger = new AuditLogger();   // or whatever constructor your project uses
-
-			AdminFrame frame = new AdminFrame(catalogService, orderService, logger);
-			frame.setVisible(true);
-		});
-	}
+            AuditLogger logger = new AuditLogger();
 
             FloralShopFrame frame = new FloralShopFrame(catalogService, orderService);
             frame.setVisible(true);
+
+            // Page starter for us admins:
+            // AdminFrame admin = new AdminFrame(catalogService, orderService, logger);
+            // admin.setVisible(true);
         });
     }
 }
