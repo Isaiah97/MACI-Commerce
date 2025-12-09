609e9a0c130eb75c3cd67dc38fa8169bbb46b196
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-08 16:27:16 -0800

    reverting changes

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,6 +1,5 @@
 package service;
 
-import model.Admin;
 
 
 /**
@@ -12,38 +11,7 @@ public class AuthService {
     // Simple demo authentication logic.
     // Change the username/password as needed.
     public boolean authenticate(String username, String password) {
-        return "admin".equals(username) && "password123".equals(password);
-    }
-
-    private void onAdminPanel(ActionEvent e) {
-    AuthService auth = new AuthService();
-    AuditLogger logger = new AuditLogger();
-
-    // build login dialog UI...
-    // get username/password from fields...
-
-    if (result == JOptionPane.OK_OPTION) {
-        String username = userField.getText();
-        String password = new String(passField.getPassword());
-
-        if (auth.authenticate(username, password)) {
-            logger.log("Admin logged in via GUI", username);
-
-            AdminFrame adminFrame = new AdminFrame(catalogService, orderService, logger);
-            adminFrame.setVisible(true);
-
-            JOptionPane.showMessageDialog(this,
-                    "Admin logged in. Admin Panel opened.",
-                    "Admin Panel",
-                    JOptionPane.INFORMATION_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(this,
-                    "Invalid admin credentials.",
-                    "Admin Panel",
-                    JOptionPane.ERROR_MESSAGE);
-                }
-        }
+        return "admin".equals(username) && "secure123".equals(password);
     }
 }
-
-
+    
